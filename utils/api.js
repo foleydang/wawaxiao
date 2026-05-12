70a48435efba7dfeda711ee548b78318c315b7fb
foleydang <719312518@aa.com>
2026-05-12 23:16:41 +0800

    fix: 修复API域名配置
    - 从 api.yanten.top 改为 yanten.top
    - API端点已全部补充完成

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://api.yanten.top/api/wawaxiao'
+const API_BASE = 'https://yanten.top/api/wawaxiao'
 
 function request(url, method = 'GET', data = {}) {
   return new Promise((resolve, reject) => {
